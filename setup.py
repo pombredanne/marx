0452b703f668003e433e9d07f8d2235583f7ec1e
Paul Tagliamonte <tag@pault.ag>
2013-11-02 22:45:06 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,25 @@
+from marx import __appname__, __version__
+from setuptools import setup
+
+
+long_description = ""
+
+setup(
+    name=__appname__,
+    version=__version__,
+    scripts=[],
+    packages=[
+        'marx',
+    ],
+    author="Paul Tagliamonte",
+    author_email="tag@pault.ag",
+    long_description=long_description,
+    description='Control the means of production',
+    license="Expat",
+    url="http://deb.io/",
+    platforms=['any'],
+    entry_points={
+        'console_scripts': [
+        ],
+    }
+)
