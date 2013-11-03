839c8d34f03f9b99087f612cfe4aab15ec14f116
Paul Tagliamonte <tag@pault.ag>
2013-11-03 17:50:44 -0500

    add where this is going

--- test.py
+++ test.py
@@ -5,5 +5,4 @@ d = Docker()
 #for x in d.rm(*[x['id'] for x in d.ps(a=True)]):
 #    print "Removed: %s" % (x)
 
-for line in d.run("ubuntu", "echo", "hello"):
-    print line
+hsh = d.run("ubuntu", "echo", "hello", d=True)
