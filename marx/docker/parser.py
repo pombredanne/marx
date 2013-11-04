1ebead24f2b453f6ee5b0f6d40d4ced78c163349
Paul Tagliamonte <tag@pault.ag>
2013-11-03 19:16:52 -0500

    foo

--- marx/docker/parser.py
+++ marx/docker/parser.py
@@ -16,8 +16,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
-
-
 def parse_list_output(stream):
     stream = iter(stream.splitlines())
     header_ = next(stream)
