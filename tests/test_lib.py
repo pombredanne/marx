6d6d5b97d6f8dfc53a6b92fd6a72b15a860c68d0
Paul Tagliamonte <tag@pault.ag>
2013-11-05 16:52:07 -0500

    do things

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -3,9 +3,19 @@ from . import resource_root
 
 
 def test_classes():
+    """ Test that classes get properly loaded """
+
     l = Lib(resource_root)
     classes = list(l.get_classes())
     assert classes == ['class1']
 
     klass = l.get_class('class1')
     assert klass == {"Hello": "World"}
+
+
+def test_dockerfiles():
+    """ Test that we can list dockerfiles """
+
+    l = Lib(resource_root)
+    files = list(l.get_dockerfiles())
+    assert files == ["TestImage"]
