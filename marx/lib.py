55694592dde2a29cac70c3c2eac528f8607e5ee9
Paul Tagliamonte <tag@pault.ag>
2013-11-05 16:44:14 -0500

    Update class getter

--- marx/lib.py
+++ marx/lib.py
@@ -1,18 +1,31 @@
+import glob
 import json
+import os
 
 
 class Lib(object):
     def __init__(self, directory):
         self.directory = directory
 
-    def _file_listing(self, path):
-        pass
+    def _json_listing(self, path):
+        flag = ".json"
+        for entry in glob.iglob(os.path.join(path, "*%s" % (flag))):
+            entry = os.path.basename(entry)
+            if entry.endswith(flag):
+                entry = entry[:-len(flag)]
+            yield entry
 
-    def _load_file(self, path):
-        pass
+    def _load_json(self, path):
+        return json.load(open(path, 'r'))
 
     def get_classes(self):
-        pass
+        return self._json_listing(os.path.join(self.directory, 'classes'))
+
+    def get_class(self, what):
+        return self._load_json(os.path.join(
+            self.directory,
+            'classes', "%s.json" % (what))
+        )
 
     def get_containers(self):
         pass
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -4,4 +4,8 @@ from . import resource_root
 
 def test_classes():
     l = Lib(resource_root)
-    print l.get_classes()
+    classes = list(l.get_classes())
+    assert classes == ['class1']
+
+    klass = l.get_class('class1')
+    assert klass == {"Hello": "World"}
